0f2e3c50d26bb0bcabd126d6c1be4ea19bcc08d8
DmyP <pechenyi.d@gmail.com>
2017-08-11 16:40:48 +0300

    Event class and EventLogger interfaces added

---
+++ src/main/java/com/ep/spring/core/Event.java
@@ -0,0 +1,32 @@
+package com.ep.spring.core;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class Event {
+    private int id;
+    private String msg;
+    private Date date;
+    private DateFormat df;
+
+    public Event(Date date, DateFormat df) {
+        this.id = (int) (Math.random() * 100);
+        this.date = date;
+        this.df = df;
+    }
+
+    public Event(String msg, Date date, DateFormat df) {
+        this.id = (int) (Math.random() * 100);
+        this.msg = msg;
+        this.date = date;
+        this.df = df;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
---
+++ src/main/java/com/ep/spring/core/EventLogger.java
@@ -0,0 +1,5 @@
+package com.ep.spring.core;
+
+public interface EventLogger {
+    void logEvent(Event event);
+}
